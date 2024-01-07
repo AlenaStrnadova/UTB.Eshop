a0ba07b27b41f8179bd7f53b5c81db186935ff0d
AlenaStrnadova <strnadova@post.cz>
2024-01-07 20:54:47 +0100

    balíčky nuget do Infrastructure, složka Database do Infratructure, do složky class EshopDbContext + změny kódu
    (R na závislosti v Eshop.Infrastructure - přidat Entity Framework Core a Pomelo vyhledané jako mysql entity framework core, R na Infrastructure - přidat - nová složka - Database, R na Database - přidat - třída - EshopDbContext - kde kód viz commit)

---
+++ UTB.Eshop.Infrastructure/Database/EshopDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UTB.Eshop.Domain.Entities;
+
+namespace UTB.Eshop.Infrastructure.Database
+{
+    public class EshopDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public EshopDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+    }
+}
