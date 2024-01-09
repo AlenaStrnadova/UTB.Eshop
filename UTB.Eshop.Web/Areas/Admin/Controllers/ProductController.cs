3641305d6c5b9898dc660e6c19eb0d51e5ca5209
AlenaStrnadova <strnadova@post.cz>
2024-01-09 16:46:05 +0100

    added/change code to web-areas-admin-views-product-create.cshtml
    add code to web-areas-admin-controllers-ProductController.cs

--- UTB.Eshop.Web/Areas/Admin/Controllers/ProductController.cs
+++ UTB.Eshop.Web/Areas/Admin/Controllers/ProductController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UTB.Eshop.Domain.Entities;
 
 namespace UTB.Eshop.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            return View();
+        }
+
     }
 }
